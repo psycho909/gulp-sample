ebea5ef0bab383bf8d2000998d921aab19bbe392
psycho909 <psychosocial909@gmail.com>
2018-08-13 11:14:04 +0800

    123

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,10 @@
-console.log("hello");
+$('.top').on('click',function(){
+    $('html,body').animate({
+        scrollTop:0
+    },300)
+})
+
+$(window).on('scroll',function(){
+
+})
+console.log(321)
